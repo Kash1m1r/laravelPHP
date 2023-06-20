f47fa248967a6101f08094cacddf194749c51324
Gabriel Pereira Gomes <05762378330@dpema.br>
2023-06-20 11:12:40 -0300

    novosarquivos

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = DB::select('SELECT nome FROM series;');
+
+
+        return view('series.index')->with('series', $series);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+    public function store(Request $request){
+        $nomeSerie = $request->input('nome');
+
+        if(DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])){
+            return redirect('/series');
+        }else{
+            return "ERRROOOR";
+        }
+    }
+}
