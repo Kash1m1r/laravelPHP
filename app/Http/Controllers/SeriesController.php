41fb5d336db74c5e52b07b91824fff51ed0daf98
Gabriel Pereira Gomes <05762378330@dpema.br>
2023-08-04 16:06:44 -0300

    novo

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\SeriesFormRequest;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = session('mensagem.sucesso');
+
+        return view('series.index')->with('series', $series)
+            ->with('mensagemSucesso', $mensagemSucesso);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        $serie = Serie::create($request->all());
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+    }
+
+    public function destroy(Serie $series)
+    {
+        $series->delete();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
+    }
+
+    public function edit(Serie $series)
+    {
+        dd($series->temporadas);
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Serie $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso");
+    }
+}
