2f806340c163bdce233a6b5877b248ec2a5bab5c
Gabriel Pereira Gomes <05762378330@dpema.br>
2023-08-08 14:52:22 -0300

    projeto

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -41,7 +41,7 @@ class SeriesController extends Controller
 
     public function edit(Serie $series)
     {
-        dd($series->temporadas);
+     
         return view('series.edit')->with('serie', $series);
     }
 
--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,6 +12,12 @@ class Serie extends Model
     protected $fillable = ['nome'];
 
     public function temporadas(){
-        return $this->hasMany(Season::class);
+        return $this->hasMany(Season::class, 'series_id');
+    }
+
+    protected static function booted(){
+        self::addGlobalScope('ordered', function (Builder $queryBuilder) {
+            $queryBuilder->orderBy('nome');
+        });
     }
 }
